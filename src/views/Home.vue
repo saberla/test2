a482e54f250eb4991aa60c923cc2cc593ca0df28
saberla <1850065377@qq.com>
2019-09-10 17:36:12 +0800

    test

---
+++ src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld :str="str" @setValue="getValue"></HelloWorld>
+    <h1>{{value}}</h1>
+    <h2>{{info.title}}</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  data() {
+    return {
+      value: [],
+      str: {
+        name: "hello",
+        age: 12,
+        last: "父向子传值"
+      },
+      info: [],
+    };
+  },
+  components: {
+    HelloWorld
+  },
+  methods: {
+    getValue(value) {
+      this.value = value;
+    }
+  },
+  mounted() {
+    this.$axios
+    .get('http://jsonplaceholder.typicode.com/posts')
+    .then((response) => {
+      console.log(response);
+      var data = response.data[0];
+      console.log(data);
+      this.info = data;
+      console.log(this);
+    })
+    .catch(function(error){
+      console.log(error);
+    })
+  },
+};
+</script>
---
+++ src/views/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>i'm a test page</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {}
+};
+</script>
+<style lang='less' scoped>
+</style>
